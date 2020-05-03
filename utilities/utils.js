d1efa896996bd4348af05ab495b3a40a08e2ea3d
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-03 00:23:53 -0700

    init commit of lab 4

--- utilities/utils.js
+++ utilities/utils.js
@@ -4,8 +4,6 @@ let pool = require("./sql_conn.js")
 //We use this create the SHA256 hash
 const crypto = require("crypto");
 
-const nodemailer = require("nodemailer");
-
 function sendEmail(from, receiver, subj, message) {
  //research nodemailer for sending email from node.
  // https://nodemailer.com/about/
@@ -15,39 +13,8 @@ function sendEmail(from, receiver, subj, message) {
  //similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
 
  //fake sending an email for now. Post a message to logs.
- //console.log('Email sent: ' + message);
-
- // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.GMAIL_USER, // generated ethereal user
-      pass: process.env.GMAIL_PASS // generated ethereal password
-    }
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: from, // sender address
-    to: receiver, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>" // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+ console.log('Email sent: ' + message);
 }
-
-sendEmail().catch(console.error);
 
 /**
  * Method to get a salted hash.
