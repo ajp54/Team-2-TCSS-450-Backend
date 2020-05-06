8e31d5f94e8c6ddb4764f2d26e277a08a0a64299
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-05 23:22:40 -0700

    reverted code

--- routes/register.js
+++ routes/register.js
@@ -63,19 +63,14 @@ router.post('/', (req, res) => {
         //If you want to read more: https://stackoverflow.com/a/8265319
         let theQuery = "INSERT INTO MEMBERS(FirstName, LastName, Username, Email, Password, Salt, Emailtoken) VALUES ($1, $2, $3, $4, $5, $6, $7)"
         let values = [first, last, username, email, salted_hash, salt, emailToken]
-        console.log('test')
         pool.query(theQuery, values)
             .then(result => {
                 //We successfully added the user, let the user know
                 res.status(201).send({
                     success: true,
-                    message: "IHEF:OIHOIEWJHFOIWIEHFGOIWHGIOUWEH:GFUIHW:GEOI",
                     email: result.rows[0].email
                 })
-                res.send({
-                    message: "hey im here lol"
-                })
-                //sendEmail("ShootTheBreeze@gmail.com", email, "Please confirm your email.", emailToken);
+                sendEmail("ShootTheBreeze@gmail.com", email, "Please confirm your email.", emailToken);
             })
             .catch((err) => {
                 //log the error
@@ -91,9 +86,6 @@ router.post('/', (req, res) => {
                 } else {
                     res.status(400).send({
                         message: err.detail
-                    })
-                    res.send({
-                        message: "asdfasdf"
                     })
                 }
             })
