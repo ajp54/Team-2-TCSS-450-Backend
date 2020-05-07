0dd7a5c482fee5793ff33c675fb5838f2026ada3
sschuler2 <57421466+sschuler2@users.noreply.github.com>
2020-05-07 00:47:40 -0700

    Update utils.js
    added tls unauthorized rejection false

--- utilities/utils.js
+++ utilities/utils.js
@@ -18,11 +18,14 @@ function sendEmail(from, receiver, subj, emailToken) {
  //console.log('Email sent: ' + message);
 
 var transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "Gmail",
   auth: {
     //type: 'Oauth2',
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
+  }
+  tls: {
+    rejectUnauthorized:false
   }
 });
 
