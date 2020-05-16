4078ec44e321036560e11023f966319d4fa24b0a
bcope0431 <copeb@uw.edu>
2020-05-15 22:37:49 -0700

    weather fixes

--- routes/weather.js
+++ routes/weather.js
@@ -15,7 +15,7 @@ var router = express.Router()
  * @apiHeader {String} a request for specified weather info
  * 
  */
-router.get("/weather", (request, response) => {
+router.get("/", (request, response) => {
 
     // hardcoded for UWT
     let url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=dc96b2428dc140f09a710254201405&q=98402&format=json&num_of_days=5`
