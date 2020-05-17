965d1f59db9c488a845b23899e1920ab9e77e237
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-17 06:42:45 -0700

    testing verify

--- routes/verify.js
+++ routes/verify.js
@@ -29,7 +29,7 @@ let config = {
 router.get("/", (request, response) => {
     if(request.query.token != null) {
       try {
-        response.send(jwt.verify(request.query.token, config.secret))
+        const { "memberid": { id } } = jwt.verify(request.query.token, config.secret)
         let theQuery = "UPDATE MEMBERS SET verification=1 WHERE memberid=$1"
         let values = [{ id }]
         response.send({ id })
