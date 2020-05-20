6a47ded02769ce85977c04d0ae44762457f0b8c7
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-19 21:35:59 -0700

    changed how contacts get processes data

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,7 +6,6 @@ let pool = require('../utilities/utils').pool
 
 var router = express.Router()
 
-const request = require('request')
 
 //This allows parsing of the body of POST requests, that are encoded in JSON
 router.use(require("body-parser").json())
@@ -21,7 +20,6 @@ router.use(require("body-parser").json())
  * 
  * @apiParam {Number} memberId the member to look up. 
  * 
- * @apiSuccess {Number} rowCount the number of chat IDs returned
  * @apiSuccess {Object[]} chatIds List of chat IDs the member is in
  * @apiSuccess {String} messages.email The email for the member in the chat
  * 
@@ -33,19 +31,77 @@ router.use(require("body-parser").json())
  * 
  * @apiUse JSONError
  */ 
-router.get("/", (request, response, next) => {
+router.get("/", (request, response) => {
     if(request.decoded != null) {
         try {
           let user = request.decoded
-          let theQuery = `SELECT Contacts.MemberID_B
-                          FROM Contacts
-                          WHERE MemberID_A=$1`
+          let theQuery = `SELECT memberid_B FROM contacts
+                          INNER JOIN members ON contacts.memberid_A=member.memberid
+                          WHERE memberid=$1`
           let values = [user.memberid]
           pool.query(theQuery, values)
                   .then(result => {
                       //We successfully update the user, let the user know
-                      response.status(201).send({
-                          verification: result.rows[0].verification
+                      response.send({
+                        rows: result.rows
+                      })
+                  })
+                  .catch((err) => {
+                      //log the error
+                      //console.log(err)
+                    response.status(400).send({
+                        message: err.detail
+                    })
+                  })
+        } catch (e) {
+          response.send(e)
+        }
+        
+      } else {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+      }
+      response.send({
+        message: "Contacts have been retrieved." 
+      })
+});
+
+/**
+ * @api {post} /contacts/post/
+ * @apiName PostContacts
+ * @apiGroup Contacts
+ * 
+ * @apiHeader {String} authorization Valid JSON Web Token JWT
+ * 
+ * @apiParam {String} otherEmail the email to add to users contacts
+ * 
+ * @apiSuccess {String} successfully added to contacts 
+ * 
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * 
+ * @apiError (400: SQL Error) {String} message the reported SQL error details
+ * 
+ * @apiUse JSONError
+ */ 
+router.post("/", (request, response) => {
+    if(request.body.otherEmail != null && request.decoded != null) {
+        try {
+          let user = request.decoded
+          let theQuery = `INSERT INTO Contacts(memberID_A, memberID_B, verified)
+                          VALUES(SELECT members.memberid 
+                                 FROM members 
+                                 WHERE members.email=$1),
+                                (SELECT members.memberid
+                                 FROM members
+                                 WHERE members.email=$2),
+                                $3)`
+          let values = [user.email, request.body.otherEmail, 0]
+          pool.query(theQuery, values)
+                  .then(result => {
+                      //We successfully update the user, let the user know
+                      response.send({
+                          message: "Successfully added to contacts."
                       })
                   })
                   .catch((err) => {
@@ -60,7 +116,67 @@ router.get("/", (request, response, next) => {
         }
         
       }
+      else {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+      }
       response.send({
         message: "Contacts have been retrieved." 
       })
 });
+
+/**
+ * @api {delete} /contacts/delete/
+ * @apiName DeleteContacts
+ * @apiGroup Contacts
+ * 
+ * @apiHeader {String} authorization Valid JSON Web Token JWT
+ * 
+ * @apiParam {String} otherEmail the email to add to users contacts
+ * 
+ * @apiSuccess {String} successfully added to contacts 
+ * 
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * 
+ * @apiError (400: SQL Error) {String} message the reported SQL error details
+ * 
+ * @apiUse JSONError
+ */ 
+router.delete("/", (request, response) => {
+    if(request.body.otherEmail != null && request.decoded != null) {
+        try {
+          let user = request.decoded
+          let theQuery = `DELETE FROM Contacts
+                          WHERE memberID_A=$1
+                          AND memberID_B=`
+          let values = [user.memberid, request.body.othermemberID]
+          pool.query(theQuery, values)
+                  .then(result => {
+                      //We successfully update the user, let the user know
+                      response.send({
+                          message: "Successfully added to contacts."
+                      })
+                  })
+                  .catch((err) => {
+                      //log the error
+                      //console.log(err)
+                    response.status(400).send({
+                        message: err.detail
+                    })
+                  })
+        } catch (e) {
+          response.send(e)
+        }
+        
+      }
+      else {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+      }
+      response.send({
+        message: "Contacts have been retrieved." 
+      })
+});
+
