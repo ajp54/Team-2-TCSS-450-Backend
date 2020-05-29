3ecf2bbd10d5bea198cf2f528568501c63876c9e
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-29 16:06:52 -0700

    testing logo in email

--- utilities/utils.js
+++ utilities/utils.js
@@ -52,7 +52,12 @@ function sendPassword(from, receiver, subj, emailToken) {
     to: receiver,
     subject: subj,
     text: 'A request was made through Shoot the Breeze to recover your password, if this was not you please ignore and/or delete this email.\n',
-    html: '<img src="ShootTheBreezeLogo.png" alt="Shoot the Breeze Logo" width="42" height="42" style="vertical-align:bottom"><p> Please click here to get a new temporary email to Shoot the Breeze: <a href="https://team-2-tcss-450-backend.herokuapp.com/temp_pass?token=' + emailToken + '">here</a> Click to get a new temporary password.</p>'
+    attachments: [{
+      filename: 'ShootTheBreezeLogo.png',
+      path: './ShootTheBreezeLogo.png',
+      cid: 'logo'
+    }],
+    html: '<img src="cid:logo" alt="Shoot the Breeze Logo" width="42" height="42" style="vertical-align:bottom"><p> Please click here to get a new temporary email to Shoot the Breeze: <a href="https://team-2-tcss-450-backend.herokuapp.com/temp_pass?token=' + emailToken + '">here</a> Click to get a new temporary password.</p>'
   };
 
   transporter.sendMail(mailOptions, function(error, info){
