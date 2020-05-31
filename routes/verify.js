d5cc3e5bc13f75e7f242bbbd748d0766bbac8014
sschule2@uw.edu <sschuler253@gmail.com>
2020-05-30 18:16:44 -0700

    added logo to emails

--- routes/verify.js
+++ routes/verify.js
@@ -39,9 +39,7 @@ router.get("/", (request, response) => {
       pool.query(theQuery, values)
               .then(result => {
                   //We successfully update the user, let the user know
-                  response.send({
-                    message: "Added"
-                  })
+                  response.send(<html> <body> <img src = {__dirname + "/</body>ShootTheBreezeLogo.png"} alt="Shoot the Breeze Logo" width="180" height="150" style="vertical-align:bottom"> </img> </body> </html>)
               })
               .catch((err) => {
                   //log the error
